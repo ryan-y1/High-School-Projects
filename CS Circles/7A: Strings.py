6aacae24b0f0c7a6ed9cf4ae0dd0c95f51677a82
Ryan Yi <63077981+ryan-y1@users.noreply.github.com>
2021-06-05 01:23:40 -0700

    Update and rename 7A: Strings to 7A: Strings.py

--- CS Circles/7A: Strings.py
+++ CS Circles/7A: Strings.py
@@ -3,11 +3,11 @@
 # Code written for the Coding Exercises of 7A: Strings
 ##
 
-String Shaving: prints the input string but without the first and last character
+# String Shaving: prints the input string but without the first and last character
 myString = input()
 print(myString[1:len(myString)-1])
 
-Heads and Tails: prints the input string but the first and last characters switch places
+# Heads and Tails: prints the input string but the first and last characters switch places
 myString = input()
 front = myString[0:1]
 body = myString[1:len(myString)-1]
@@ -15,7 +15,7 @@ end = myString[len(myString)-1:len(myString)-0]
 final = (end + body + front)
 print(final)
 
-Next Letter: prints the character that comes after the input character in the alphabet
+# Next Letter: prints the character that comes after the input character in the alphabet
 charInitial = ord(input())
 if charInitial >= 90:
    charInitial = (90 - 26)
@@ -23,12 +23,12 @@ charNext = charInitial + 1
 charFinal = chr(charNext)
 print(charFinal)
 
-Pig Latin: translates a word into pig latin and prints it out
+# Pig Latin: translates a word into pig latin and prints it out
 word = input()
 pigLatin = word[1:len(word)-0] + word[0:1] + "ay"
 print(pigLatin)
 
-The Name Game: takes a name as string input and inserts it when printing out the Name Game
+# The Name Game: takes a name as string input and inserts it when printing out the Name Game
 name = input()
 print((name + ", ") * 2 + "bo-b" + name[1:len(name)-0])
 print("banana-fana fo-f" + name[1:len(name)-0])
